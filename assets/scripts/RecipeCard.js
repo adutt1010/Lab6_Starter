620e3dfea804e9f8fb19509e34f72256d18dbcac
adutt1010 <adutt@ucsd.edu>
2025-05-13 21:03:43 -0700

    yoo lol

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -8,10 +8,66 @@ class RecipeCard extends HTMLElement {
 
 		// EXPOSE - START (All expose numbers start with A)
 		// A1. TODO - Attach the shadow DOM to this Web Component (leave the mode open)
+		this.shadow = this.attachShadow({ mode: 'open' });
 		// A2. TODO - Create an <article> element - This will hold our markup once our data is set
-		// A3. TODO - Create a style element - This will hold all of the styles for the Web Component
-		// A4. TODO - Insert all of the styles from cardTemplate.html into the <style> element you just made (copy everything INSIDE the <style> tag>)
+		this.articleElement = document.createElement('article');
+
+		const styleElement = document.createElement('style');		// A3. TODO - Create a style element - This will hold all of the styles for the Web Component
+		styleElement.textContent = `
+        * {
+            font-family: sans-serif;
+            margin: 0;
+            padding: 0;
+        }
+        article {
+            border: 1px solid #d1d1d1;
+            border-radius: 8px;
+            overflow: hidden;
+            width: 178px;
+            box-shadow: 2px 2px 5px rgba(0,0,0,0.15);
+        }
+        article img {
+            width: 100%;
+        }
+        p {
+            padding: 0 16px;
+        }
+        p.title {
+            font-weight: bold;
+            font-size: 16px;
+            margin-top: 8px;
+            color: #333;
+        }
+        p.organization {
+            color: #aaa;
+            font-size: 14px;
+        }
+        div.rating {
+            display: flex;
+            align-items: center;
+            padding: 8px 16px;
+        }
+        div.rating img {
+            width: 18px;
+            height: 18px;
+            margin-right: 5px;
+        }
+        time {
+            padding-left: 16px;
+            color: #888;
+            font-size: 14px;
+        }
+        p.ingredients {
+            padding: 0 16px 16px;
+            color: #555;
+            font-size: 14px;
+        }
+        `;// A4. TODO - Insert all of the styles from cardTemplate.html into the <style> element you just made (copy everything INSIDE the <style> tag>)
+		
+		
 		// A5. TODO - Append the <style> and <article> elements to the Shadow DOM
+		this.shadow.appendChild(styleElement);
+		this.shadow.appendChild(this.articleElement);
 	}
 
 	/**
@@ -40,6 +96,7 @@ class RecipeCard extends HTMLElement {
 		if (!data) return;
 
 		// A6. TODO - Select the <article> we added to the Shadow DOM in the constructor
+		const article = this.articleElement;
 		// A7. TODO - Set the contents of the <article> with the <article> template given in
 		//           cardTemplate.html and the data passed in (You should only have one <article>,
 		//           do not nest an <article> inside another <article>). You should use template
@@ -47,8 +104,25 @@ class RecipeCard extends HTMLElement {
 		// 			 Do NOT include the <article> tags within the innerHTML of the element you create.
 		//           Remember to replace all the placeholders in the template with the data passed in.
 		//           i.e. imgSrc, titleLnk, etc
+		article.innerHTML = `
+			<img src="${data.imgSrc}" alt="${data.imgAlt}">
+			<p class="title">
+				<a href="${data.titleLnk}" target="_blank">${data.titleTxt}</a>
+			</p>
+			<p class="organization">${data.organization}</p>
+			<div class="rating">
+				<img src="./assets/images/icons/${data.rating}-star.svg" alt="${data.rating} stars">
+				<span>${data.rating}</span>
+				<span>(${data.numRatings})</span>
+			</div>
+			<time>${data.lengthTime}</time>
+			<p class="ingredients">
+				${data.ingredients}
+			</p>
+		`;
 	}
 }
 
 // A8. TODO - Define the Class as a customElement so that you can create
 //           'recipe-card' elements
+customElements.define('recipe-card', RecipeCard);
